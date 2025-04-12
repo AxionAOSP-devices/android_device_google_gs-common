aca1b3d83916ab56da20f41491c445ad6b947cef
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-12 22:31:26 +0800

    gs-common: enable VoLTE
    Change-Id: I21583f59b24a531c2d3540c99ae1e75a1e855381
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -24,6 +24,17 @@ PRODUCT_SOONG_NAMESPACES += \
 # Disable OMX
 PRODUCT_PROPERTY_OVERRIDES += \
 	vendor.media.omx=0
+	
+# Enable VoLTE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.radio.rat_on=combine \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
